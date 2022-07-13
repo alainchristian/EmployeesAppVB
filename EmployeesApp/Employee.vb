acbf705ad08c601012724cf2140e2e3bcda075e0
Alain <alchris0724@gmail.com>
2022-07-13 12:44:19 +0200

    Add project files.

---
+++ EmployeesApp/Employee.vb
@@ -0,0 +1,26 @@
+﻿Public Class Employee
+    Public EmpId As String
+    Public EmpName As String
+    Public EmpAddress As String
+    Public SalaryJune As Decimal
+    Public SalaryJuly As Decimal
+    Public Function TotSal() As Decimal
+        Return SalaryJuly + SalaryJune
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return EmpId & vbTab & EmpName & vbTab & EmpAddress & vbTab & SalaryJune & vbTab & SalaryJuly & vbTab & TotSal()
+    End Function
+    Public Sub New(EmpId As String, EmpName As String, EmpAddress As String, SalaryJune As Decimal, SalaryJuly As Decimal)
+        Me.EmpId = EmpId
+        Me.EmpName = EmpName
+        Me.EmpAddress = EmpAddress
+        Me.SalaryJune = SalaryJune
+        Me.SalaryJuly = SalaryJuly
+    End Sub
+    Public Sub New()
+
+    End Sub
+
+
+End Class
---
+++ EmployeesApp/Module1.vb
@@ -0,0 +1,59 @@
+﻿Module Module1
+    Dim empList As List(Of Employee) = New List(Of Employee)
+    Sub Main()
+        Dim choice As String
+        'choice = ""
+        Do
+            Console.Write("1.Add Employee " & vbNewLine & "2.Display Employee" & vbNewLine & vbNewLine & "Choose an option: ")
+            choice = Console.ReadLine()
+            Select Case choice
+                Case "1"
+                    AddEmployees()
+                Case "2"
+                    DisplayEmployees()
+                Case Else
+                    Console.Write("Invalid choice")
+
+            End Select
+        Loop While choice <> "n"
+        Console.ReadLine()
+    End Sub
+    Sub AddEmployees()
+        Dim id, name, address As String
+        Dim june, july As Decimal
+        id = ""
+        Do
+            Console.Write("Employee Id:")
+            id = Console.ReadLine()
+            If id = "" Then
+                Exit Sub
+            End If
+            Console.Write("Employee Name:")
+            name = Console.ReadLine()
+            Console.Write("Employee Address:")
+            address = Console.ReadLine()
+            Console.Write("Salary June:")
+            june = Console.ReadLine()
+            Console.Write("Salary July:")
+            july = Console.ReadLine()
+
+            empList.Add(New Employee(id, name, address, june, july))
+
+        Loop While id <> ""
+    End Sub
+    Sub DisplayEmployees()
+        Dim totJune, totJuly As Decimal
+        totJune = 0
+        totJuly = 0
+        For Each emp In empList
+            totJune = totJune + emp.SalaryJune
+            totJuly = totJune + emp.SalaryJuly
+            Console.WriteLine(emp)
+
+        Next
+        Console.WriteLine("Total Salary in June: " & totJune)
+        Console.WriteLine("Total Salary in July: " & totJuly)
+        Console.ReadLine()
+    End Sub
+
+End Module
